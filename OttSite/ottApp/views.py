f8b785982a2e1038618e85cc7aaf59898a4fdcd7
Tim Lyons <amazingasriel2001@gmail.com>
2022-10-24 13:03:04 -0700

    Started on Web Pages. Base is nearly finished

--- OttSite/ottApp/views.py
+++ OttSite/ottApp/views.py
@@ -1,8 +1,16 @@
+from turtle import title
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-
     context = {
+        "title" : "Otter Den",
     }
     return render(request, "index.html", context=context)
+
+#! Remove after pages have been setup
+def ex(request):
+    context = {
+        "title" : "Otter Den",
+    }
+    return render(request, "FoundationEx.html", context=context)
