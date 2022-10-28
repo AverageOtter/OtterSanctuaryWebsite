5399e292b38957c72503b3a3396f6dd15a4eeeca
Tim Lyons <amazingasriel2001@gmail.com>
2022-10-27 19:12:23 -0700

    Created every page, still need to test login procedure. Thinking about doing 3 level system: unreg, basic, staff...

--- OttSite/ottApp/urls.py
+++ OttSite/ottApp/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('FoundationEx', views.ex, name='FoundationEx'), #!Remove
+    path("", views.index, name="index"),
+    path("aboutus/", views.aboutUs, name="aboutUs"),
+    path("otterDB/", views.otterDB, name="otterDB"),
+    path("chat/", views.chat, name="chat"),
+    path("login/", auth_views.LoginView.as_view()),
+    path("register/", views.register),
+    path("FoundationEx/", views.ex, name="FoundationEx"),
+    path("game", views.game, name="game"),  #!Remove
+    path("logout/", views.logout_user),
 ]
+
+#! If going going anywhere but homepage to site, it adds onto the current url and doesnt connect.
+#! like .../chat/login/ which isnt valid.
--- OttSite/ottApp/views.py
+++ OttSite/ottApp/views.py
@@ -1,16 +1,89 @@
 from turtle import title
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from . import models, forms
 
 # Create your views here.
 def index(request):
+    current_user = request.user
     context = {
-        "title" : "Otter Den",
+        "title": "Otter Region",
+        "current_user": current_user,
     }
     return render(request, "index.html", context=context)
 
+
+def aboutUs(request):
+    current_user = request.user
+    context = {
+        "title": "Otter Staff",
+        "current_user": current_user,
+    }
+    return render(request, "aboutus.html", context=context)
+
+
+def otterDB(request):
+    current_user = request.user
+    context = {
+        "title": "Otter Den",
+        "current_user": current_user,
+    }
+    return render(request, "otterDB.html", context=context)
+
+
+@login_required
+def chat(request):
+    current_user = request.user
+    context = {
+        "title": "Chat",
+        "current_user": current_user,
+    }
+    return render(request, "chat.html", context=context)
+
+
+def chatEmp(request):
+    current_user = request.user
+    context = {
+        "title": "Chat Elevated",
+        "current_user": current_user,
+    }
+    return render(request, "chatEmp.html", context=context)
+
+
+def game(request):
+    current_user = request.user
+    context = {
+        "title": "Game",
+        "current_user": current_user,
+    }
+    return render(request, "game.html", context=context)
+
+
+def register(request):
+    if request.method == "POST":
+        form = forms.CustomUserCreationForm()
+        if form.is_valid():
+            form.save(request)
+            # messages.success(request, 'Account created successfully')
+            return redirect("/login/")
+
+    else:
+        form = forms.CustomUserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/register.html", context=context)
+    
+
+def logout_user(request):
+    logout(request)
+    return redirect("/login/")
+
+
 #! Remove after pages have been setup
 def ex(request):
+    current_user = request.user
     context = {
-        "title" : "Otter Den",
+        "title": "Example",
+        "current_user": current_user,
     }
     return render(request, "FoundationEx.html", context=context)
